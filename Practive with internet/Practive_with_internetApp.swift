6023bacc30092e369cafa3754393050cf2a7d05c
Barry Barron <barrybbarron@gmail.com>
2022-10-01 13:28:19 -0700

    Initial Commit

---
+++ Practive with internet/Practive_with_internetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practive_with_internetApp.swift
+//  Practive with internet
+//
+//  Created by Barry Barron on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Practive_with_internetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
